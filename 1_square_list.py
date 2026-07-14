03730fc81011fe5e61b4fc7b68fb503c291ab9a7
ishapixel <ishapatil142810@gmail.com>
2026-07-14 23:09:23 +0530

    Create 1_square_list.py

---
+++ 1_square_list.py
@@ -0,0 +1,7 @@
+#program to square all numbers in a list
+numbers = [2,4,6,8,10,12]
+square_numbers=[]
+for i in numbers:
+  square_numbers.append(i*i)
+print ("original list:",numbers)
+print("square list:",square_numbers)
