16ca4823deb6d0b9e1a33cf1b90eee2ee3035b7a
ishapixel <ishapatil142810@gmail.com>
2026-07-15 17:02:11 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,11 @@
+# Program to convert a list of integers into strings
+
+numbers = [10,20,30,40,50]
+
+string_list = []
+
+for i in numbers:
+    string_list.append(str(i))
+
+print("Original List:", numbers)
+print("String List:", string_list)
